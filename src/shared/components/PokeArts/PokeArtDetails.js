b61a0e4c32272ead9505a3833cde646abe3a3295
Moutella <joaogabrielmsc@gmail.com>
2020-07-07 17:40:23 -0300

    remove fast-average-color

--- src/shared/components/PokeArts/PokeArtDetails.js
+++ src/shared/components/PokeArts/PokeArtDetails.js
@@ -3,23 +3,13 @@ import React from "react";
 import TypeIcon from "../pokemon/TypeIcon";
 import "./PokeArtDetails.css";
 import "../pokemon/Types.css"
-import FastAverageColor from "fast-average-color";
 
 const PokemonDetails = (props) => {
   let chosenArt = props.chosenArt;
   let localData = props.localData;
   console.log(chosenArt);
   console.log(props.apiData);
-  let fac = new FastAverageColor();
-  let style;
-  fac
-    .getColorAsync(process.env.REACT_APP_API_URL + chosenArt.filePath)
-    .then((color) => {
-      console.log("hmm");
-      style = {
-        color: color,
-      };
-    });
+  
   return (
     <React.Fragment>
       <div className="pokemon-details__main-art">
@@ -42,7 +32,6 @@ const PokemonDetails = (props) => {
           <img
             className={"pokemon-details__img " + localData.type[0]}
             src={process.env.REACT_APP_API_URL + chosenArt.filePath}
-            style={style}
             // src="https://upload.wikimedia.org/wikipedia/commons/e/e0/Long_March_2D_launching_VRSS-1.jpg"
             alt={localData.name.english}
           />
