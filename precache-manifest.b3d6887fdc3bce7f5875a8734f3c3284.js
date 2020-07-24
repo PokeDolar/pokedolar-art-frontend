9d86c399ff88b3b515d1b519061cd6cad688ba94
Moutella <joaogabrielmsc@gmail.com>
2020-07-24 19:28:36 -0300

    Updates

---
+++ precache-manifest.b3d6887fdc3bce7f5875a8734f3c3284.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "068d96cf50abb3160d0772886283bb9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2ce0683ce97839562d8",
+    "url": "/static/css/main.6b20878e.chunk.css"
+  },
+  {
+    "revision": "3d63b4840e6d6e536239",
+    "url": "/static/js/2.49143aca.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.49143aca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2ce0683ce97839562d8",
+    "url": "/static/js/main.89939e31.chunk.js"
+  },
+  {
+    "revision": "14dfe26a77900e4315aa",
+    "url": "/static/js/runtime-main.bfdb3855.js"
+  }
+]);
